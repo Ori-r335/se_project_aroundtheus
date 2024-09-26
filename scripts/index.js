00427b6002caef541ec434e1d679c1f42009cdcb
ORI <gl33556@gmail.com>
2024-09-26 16:30:26 +0300

    task 1-2 of final stage

--- scripts/index.js
+++ scripts/index.js
@@ -30,23 +30,41 @@ const initialCards = [
   },
 ];
 
-
-
 const editBtnClick = document.querySelector(".profile__edit-button");
 const profileEditModal = document.querySelector(".modal");
 const editCloseBtn = profileEditModal.querySelector(".modal__close-button");
+const profileSection =document.querySelector('.profile');
+const profileName = profileSection.querySelector('.profile__title');
+const profileSubtitle = profileSection.querySelector('.profile__subtitle');
+const inputName = profileEditModal.querySelector('#name');
+const inputSubtitle = profileEditModal.querySelector('#subtitle');
+const profileFormSubmit = profileEditModal.querySelector('.modal__form');
 
-editBtnClick.addEventListener('click', editClick);
- 
-function editClick(){
+
+
+editBtnClick.addEventListener("click", editClick);
+function editClick() {
+  inputName.value = profileName.textContent;
+  inputSubtitle.value = profileSubtitle.textContent;
   profileEditModal.classList.add("modal_opened");
- };
+}
 
- 
- editCloseBtn.addEventListener('click', editClickClose);
+function closePopup(){
+  profileEditModal.classList.remove("modal_opened");
+}
 
-  function editClickClose() {
-   profileEditModal.classList.remove("modal_opened");
-  };
+editCloseBtn.addEventListener("click", editClickClose);
+function editClickClose() {
+  closePopup()
+}
 
+
+profileFormSubmit.addEventListener('submit', handleFormSubmit);
+ function handleFormSubmit(evt) {
+   evt.preventDefault();
+   profileName.textContent = inputName.value;
+   profileSubtitle.textContent = inputSubtitle.value;
+   closePopup()
+  }
+  
 
