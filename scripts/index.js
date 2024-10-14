6ed90088cc9d4c69c64b141a21def15daef1dd7e
ORI <gl33556@gmail.com>
2024-10-14 16:43:41 +0300

    sprint 5 level 1 and 2

--- scripts/index.js
+++ scripts/index.js
@@ -30,58 +30,91 @@ const initialCards = [
   },
 ];
 
+//specifice modal selector
+const profileModalEdit = document.querySelector("#modal-edit");
+const profileModalAdd = document.querySelector("#modal-add");
+
+// open buttons
 const editBtnClick = document.querySelector(".profile__edit-button");
-const profileEditModal = document.querySelector(".modal");
-const editCloseBtn = profileEditModal.querySelector(".modal__close-button");
-const profileSection =document.querySelector('.profile');
-const profileName = profileSection.querySelector('.profile__title');
-const profileSubtitle = profileSection.querySelector('.profile__subtitle');
-const inputName = profileEditModal.querySelector('#name');
-const inputSubtitle = profileEditModal.querySelector('#subtitle');
-const profileFormSubmit = profileEditModal.querySelector('#modal-form');
+const addNewCardBtn = document.querySelector(".profile__add-button");
+
+// close button
+const editCloseBtn = profileModalEdit.querySelector(".modal__close-button");
+const addCloseBtn = profileModalAdd.querySelector(".modal__close-button");
+
+//text in the edit form
+const profileSection = document.querySelector(".profile");
+const profileName = profileSection.querySelector(".profile__title");
+const profileSubtitle = profileSection.querySelector(".profile__subtitle");
+const inputName = profileModalEdit.querySelector("#name");
+const inputSubtitle = profileModalEdit.querySelector("#subtitle");
+
+// form
+const profileFormSubmitEdit =
+  profileModalEdit.querySelector("#modal-form-edit");
+const profileFormSubmitAdd = profileModalAdd.querySelector("#modal-form-add");
+
+//cards
 const cardList = document.querySelector(".cards__list");
 const template = document.querySelector("#card-template").content;
 const cardTemplate = template.querySelector(".card");
 
+//------------------functions of forms----------------------//
 
+function modalClick(modal) {
+  modal.classList.add("modal_opened");
+}
 
-editBtnClick.addEventListener("click", editClick);
-function editClick() {
+function closePopup(modal) {
+  modal.classList.remove("modal_opened");
+}
+
+//---------------------------------------------------------//
+
+//------------------EventListeners of forms----------------//
+
+editBtnClick.addEventListener("click", () => {
   inputName.value = profileName.textContent;
   inputSubtitle.value = profileSubtitle.textContent;
-  profileEditModal.classList.add("modal_opened");
+  modalClick(profileModalEdit);
+});
+
+addNewCardBtn.addEventListener("click", () => {
+  modalClick(profileModalAdd);
+});
+
+editCloseBtn.addEventListener("click", () => {
+  closePopup(profileModalEdit);
+});
+
+addCloseBtn.addEventListener("click", () => {
+  closePopup(profileModalAdd);
+});
+
+// submit of edit form :
+profileFormSubmitEdit.addEventListener("submit", handleFormSubmit);
+
+//-------------------------------------------------------//
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileSubtitle.textContent = inputSubtitle.value;
+  closePopup(profileModalEdit);
 }
 
-function closePopup(){
-  profileEditModal.classList.remove("modal_opened");
-}
-
-editCloseBtn.addEventListener("click", closePopup);
-
-
-profileFormSubmit.addEventListener('submit', handleFormSubmit);
- function handleFormSubmit(evt) {
-   evt.preventDefault();
-   profileName.textContent = inputName.value;
-   profileSubtitle.textContent = inputSubtitle.value;
-   closePopup()
-  }
-  
-
-function getCardElement (data){
-   
-  const cardElement = cardTemplate.cloneNode(true);  
+function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true);
   const cardImage = cardElement.querySelector(".card__image");
   const cardTitle = cardElement.querySelector(".card__title");
-  
-  cardImage.src = data.link;  
+
+  cardImage.src = data.link;
   cardImage.alt = data.name;
   cardTitle.textContent = data.name;
   return cardElement;
 }
 
-initialCards.forEach((data)=>{
-    const cardElement = getCardElement(data);
-    cardList.append(cardElement);
+initialCards.forEach((data) => {
+  const cardElement = getCardElement(data);
+  cardList.append(cardElement);
 });
-
